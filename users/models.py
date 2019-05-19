4bd8a4d352a8afc8c3fb6cf9fe7b8c414ba4e894
vogonyo <vogonyo101@gmail.com>
2019-05-19 09:03:06 -0700

    Changed Profile __str__

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return print(self.user.username  + 'Profile')
    
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
