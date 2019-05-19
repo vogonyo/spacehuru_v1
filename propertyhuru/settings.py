43649248da80ed27e2cd6c2787d9c723d5f0ca36
vogonyo <vogonyo101@gmail.com>
2019-05-19 11:19:18 -0700

    final commit on static root

--- propertyhuru/settings.py
+++ propertyhuru/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT=os.path.join(BASE_DIR, 'propertyhuru/static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-      os.path.join(BASE_DIR, 'propertyhuru/static')
+     # os.path.join(BASE_DIR, 'propertyhuru/static')
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
