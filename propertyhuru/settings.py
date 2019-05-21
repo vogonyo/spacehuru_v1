b4617866fdb31c1b6f9b2b07ea876303f3b56425
vogonyo <vogonyo101@gmail.com>
2019-05-21 04:05:44 -0700

    undoing context_processors

--- propertyhuru/settings.py
+++ propertyhuru/settings.py
@@ -70,7 +70,6 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.core.context_processors.static',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
